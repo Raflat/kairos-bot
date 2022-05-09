2091a4245bc3157978c0e2e93814ada430b40e4f
Raflat <adouby01@gmail.com>
2022-05-09 11:07:21 +0200

    Added project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Kairos Bot
+Bot that automatically reserves classes in Kairos.
+
+## Requirements
+- [Python](https://www.python.org/downloads/)
+- [Selenium](https://selenium-python.readthedocs.io/installation.html)
+- [Google Chrome](https://www.google.it/intl/it/chrome/?brand=YTUH&gclid=Cj0KCQjw06OTBhC_ARIsAAU1yOUYIhacb843C3kCcURBrOwZqTcQtrsoXTHGfm3OD9JWVXxOyGuuXEUaAuyqEALw_wcB&gclsrc=aw.ds)
+
+## Setup
+1. Find out what your Chrome version is by typing "chrome://settings/help" in the address bar.
+2. Download the driver that matches your Chrome version from [here](https://chromedriver.chromium.org/downloads).
+3. Set the path to the driver you just downloaded in the "DriverPath" field of the config.ini file
+4. Set the fields "Username" and "Password" of the config.ini file
---
+++ src/bot.py
@@ -0,0 +1,74 @@
+"""Automatic class reservation bot for Kairos"""
+
+from configparser import ConfigParser
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Load configuration from the .ini file
+config = ConfigParser()
+config.read(r"resources\config.ini")
+
+# Create driver for Chrome
+driver = webdriver.Chrome(service=Service(config["DEFAULT"]["DriverPath"]))
+
+
+def click(xpath):
+    """
+    Clicks on the interactables from the given list.
+
+    Parameter:
+        xpath (str): XPath of the interactables
+    """
+
+    elements = driver.find_elements(By.XPATH, xpath)
+    for element in elements:
+        element.click()
+
+
+def write(xpath, text):
+    """
+    Writes some text in the given form.
+
+    Parameters:
+        xpath (str): XPath of the interactable
+
+        text (str): text to write
+    """
+
+    driver.find_element(By.XPATH, xpath).send_keys(text)
+
+
+def main():
+    """Main function"""
+
+    # Set implicit wait to be sure that everything loads on the page
+    driver.implicitly_wait(3)
+
+    # Load Kairos in the current session
+    kairos_url = ("https://kairos.unifi.it/agendaweb/index.php?view="
+                  "prenotalezione&include=prenotalezione_home&_lang=it")
+    driver.get(kairos_url)
+
+    # Accept terms
+    click("//div[3]/div[2]/label/span[@class='slider round']")
+    click("//div[4]/div[2]/label/span[@class='slider round']")
+    click("//*[@id='oauth_btn']")
+
+    # Log in to Kairos
+    write("//*[@id='username']", config["DEFAULT"]["Username"])
+    write("//*[@id='password']", config["DEFAULT"]["Password"])
+    click("/html/body/div/div/div/div[1]/form/div[5]/button")
+
+    # Open tab for new reservations
+    click("//*[@id='menu_container']/div[1]/div/div[1]/a")
+
+    # Click on available reservations
+    click("//*[@title='Verifica e prenota il tuo posto']")
+
+    # Quit the current session
+    driver.quit()
+
+
+if __name__ == "__main__":
+    main()
