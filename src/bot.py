bc542eefd862aca21e9fb8130729fc1c1fe0f869
Raflat <adouby01@gmail.com>
2022-05-09 12:21:50 +0200

    Headless mode corrected

--- src/bot.py
+++ src/bot.py
@@ -11,7 +11,8 @@ config.read(r"resources\config.ini")
 
 # Create driver for Chrome in headless mode, to prevent user inputs while running
 options = Options()
-options.headless = True
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(service=Service(config["DEFAULT"]["DriverPath"]), options=options)
 
 def click(xpath):
